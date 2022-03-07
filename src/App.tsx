6551fad887782f46d642494bd65a4b84e17d040b
AkinaChinami <l.vong@hotmail.fr>
2022-03-07 23:17:37 +0100

    remise à niveau projet

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Services} from "./Services";
+import {World} from "../world";
 
 function App() {
+    const [services, setServices] = useState(new Services(""))
+    const [world, setWorld] = useState(new World())
+
+    useEffect(() => {
+        let services = new Services("username")
+        setServices(services)
+        services.getWorld().then(response => {
+            setWorld(response.data)
+        })
+    }, [])
+
   return (
+      /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +34,27 @@ function App() {
         </a>
       </header>
     </div>
+      */
+      <div>
+        <div className="header">
+          <div> logo monde</div>
+          <div> argent</div>
+          <div> multiplicateur</div>
+          <div> ID du joueur</div>
+        </div>
+        <div className="main">
+          <div> liste des boutons de menu</div>
+          <div className="product">
+            <div> premier produit</div>
+            <div> second produit</div>
+            <div> troisième produit</div>
+            <div> quatrième produit</div>
+            <div> cinquième produit</div>
+            <div> sixième produit</div>
+          </div>
+        </div>
+      </div>
+
   );
 }
 
