8e962b8092f12c1f749cfd55422c8b083ee18378
Quentin <quentinjust9&@gmail.com>
2022-03-14 00:28:33 +0100

    avancé1

--- src/Manager.tsx
+++ src/Manager.tsx
@@ -1,6 +1,7 @@
-import {Button} from "@mui/material";
+import {Alert, Button, IconButton, Snackbar} from "@mui/material";
 import {Pallier, World} from "./world";
 import {Services} from "./Services";
+import {useState} from "react";
 
 
 type ManagerProps = {
@@ -11,17 +12,19 @@ type ManagerProps = {
 }
 export default function ManagerComponent({services,world,hideManager}:ManagerProps){
 
+    const [open, setOpen] = useState(false);
+
     function hireManager(manager: Pallier) {
         if(world.money > manager.seuil){
             world.money=world.money-manager.seuil
             manager.unlocked=true
-
+            setOpen(true)
         }
-
     }
 
+
     return (
-            <div className="modal">
+            <div id={"font"} className="modal">
                 <div>
                     <h1 className="title">Managers make you feel better !</h1>
                 </div>
@@ -31,7 +34,7 @@ export default function ManagerComponent({services,world,hideManager}:ManagerPro
                     <div key={manager.idcible} className="managergrid">
                         <div>
                             <div className="logo">
-                                <img alt="manager logo" className="round" src= { services.server + manager.logo} />
+                                <img alt="manager logo" className="managerIcon" src= { services.server + manager.logo} />
                             </div>
                         </div>
                         <div className="infosmanager">
@@ -41,12 +44,14 @@ export default function ManagerComponent({services,world,hideManager}:ManagerPro
                         </div>
                         <div onClick={() => hireManager(manager)}>
                             <Button disabled={world.money < manager.seuil}> Hire !</Button>
+                            <Snackbar open={open} autoHideDuration={6000} >
+                                <Alert  severity="success" sx={{ width: '100%' }}>
+                                    You have a new manager !!
+                                </Alert>
+                            </Snackbar>
                         </div>
                     </div>)}
                 </div>
                 <Button className="closebutton" onClick={hideManager}> Close </Button>
             </div>
 )}
-
-export class Manager {
-}
--- src/Unlock.tsx
+++ src/Unlock.tsx
@@ -1,42 +1,38 @@
 import {Button} from "@mui/material";
-import {World} from "./world";
+import {Product} from "./world";
 import {Services} from "./Services";
 
 
 type upgradeProps = {
-    world:World
+    prod:Product
     services :Services
     afficheUnlock():void
     hideUnlock():void
 }
-export default function UnlockComponent({services,world,hideUnlock}:upgradeProps){
+export default function UnlockComponent({services,prod,hideUnlock}:upgradeProps){
 
     return (
-        <div className="modal">
+        <div id={"font"} className="modal">
             <div>
                 <h1 className="title">Unlock !</h1>
             </div>
             <div>
-                {world.upgrades.pallier.filter( upgrade => !upgrade.unlocked).map(
-                    upgrade =>
-                        <div key={upgrade.idcible} className="cashUpgradegrid">
+                {prod.palliers.pallier.filter(pallier => !pallier.unlocked).map(
+                    pallier =>
+                        <div key={pallier.idcible} className="unlock">
                             <div>
                                 <div className="logo">
-                                    <img alt="cashUpgrade logo" className="round" src= { services.server + upgrade.logo} />
+                                    <img alt="unlock logo" className="unlockIcon" src= { services.server + pallier.logo} />
                                 </div>
                             </div>
-                            <div className="infoscashUpgrade">
-                                <div className="cashUpgradename"> {upgrade.name} </div>
-                                <div className="cashUpgradecible"> {world.upgrades.pallier[upgrade.idcible-1].name } </div>
-                                <div className="cashUpgradecost"> {upgrade.seuil} </div>
+                            <div className="infoscashUnlock">
+                                <div className="Unlockname"> {pallier.name} </div>
+                                <div className="Unlockcost"> {pallier.seuil} </div>
+                                <div className={"Unlocktype"}> {pallier.ratio +pallier.typeratio}</div>
                             </div>
-                            {/*<div onClick={() => (manager)}>*/}
-                            {/*    <Button disabled={world.money < manager.seuil}> Hire !</Button>*/}
-                            {/*</div>*/}
                         </div>)}
             </div>
             <Button className="closebutton" onClick={hideUnlock}> Close </Button>
         </div>
     )}
 
-export class Unlock {}
