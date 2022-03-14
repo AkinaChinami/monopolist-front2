09412cb469648b022fd38ad3bbf4bb3177d5bead
Quentin <quentinjust9&@gmail.com>
2022-03-14 09:54:44 +0100

    avancé front 3

--- src/App.tsx
+++ src/App.tsx
@@ -174,7 +174,7 @@ export default function App() {
                 <span id = "font" className="score">
                     <span dangerouslySetInnerHTML={{__html: transform(world.score)}}/>
                 </span>
-                <Button type="button" id = "commutateur" onClick={changeMult}>X{qtmulti}</Button>
+                <Button  className = "commutateur" onClick={changeMult}>X{qtmulti}</Button>
             </div>
 
             <div className="main">
